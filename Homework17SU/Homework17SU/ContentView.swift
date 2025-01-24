08ed0491c9274ecfbb2630d7a8c6f931de88cb0d
RuslanWorkGit <lulka758@gmail.com>
2025-01-24 14:55:47 +0200

    Done task with DCP
    create DispatchGroup and 4 concurrent async operation, and at the end change Text on the screen

--- Homework17SU/Homework17SU/ContentView.swift
+++ Homework17SU/Homework17SU/ContentView.swift
@@ -9,16 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @StateObject var text = "Text"
+    @State private var text = "Text"
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Text(text)
+            Button("GCP") {
+                gcpStart()
+            }
+            .font(.title3)
+            .foregroundColor(.white)
+            .frame(width: 150, height: 50)
+            .background(.blue)
+            .cornerRadius(20)
+            .padding(10)
+            
+
+            
+            Button("Actor") {
+                
+            }
+            .font(.title3)
+            .foregroundColor(.white)
+            .frame(width: 150, height: 50)
+            .background(.blue)
+            .cornerRadius(20)
+            .padding(10)
+
         }
         .padding()
+    }
+    
+    func gcpStart() {
+        
+        let group = DispatchGroup()
+        
+        group.enter()
+        DispatchQueue.global(qos: .userInteractive).async {
+            var counter = 0
+            while counter <= 10000 {
+                counter += 1
+                print("🐶 - User Interactive, iteration - \(counter)")
+            }
+            group.leave()
+        }
+        
+        group.enter()
+        DispatchQueue.global(qos: .userInitiated).async {
+            var counter = 0
+            while counter <= 10000 {
+                counter += 1
+                print("🐸 - User Initiated, iteration - \(counter)")
+            }
+            group.leave()
+        }
+        
+        group.enter()
+        DispatchQueue.global(qos: .utility).async {
+            var counter = 0
+            while counter <= 10000 {
+                counter += 1
+                print("🐒 - Utility, iteration - \(counter)")
+            }
+            group.leave()
+        }
+        
+        group.enter()
+        DispatchQueue.global(qos: .background).async {
+            var counter = 0
+            while counter <= 10000 {
+                counter += 1
+                print("🐳 - Background, iteration - \(counter)")
+            }
+            group.leave()
+        }
+     
+        
+        group.notify(queue: DispatchQueue.main) {
+            text = "Done task"
+        }
+        
     }
 }
 
